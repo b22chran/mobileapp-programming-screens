98d6d66ba4eeb2362a955ac71eeb8f47ed654d2a
Christian Antonsson <antonsson.christian@live.se>
2023-04-20 11:52:59 +0200

    Lagt till mottagandet av text-strängen från activity 1 till activity 2 som presenterar den i en "textView" på skärmen.

--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -2,17 +2,25 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 
 public class MainActivity2 extends AppCompatActivity {
     TextView textOutput;
-
+    String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         textOutput = findViewById(R.id.textView);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            name = extras.getString("name");
+            // Do something with the name and number
+        }
+        textOutput.setText(name);
     }
 }
