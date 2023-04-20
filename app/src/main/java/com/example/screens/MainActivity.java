ddfc39d5a48c308cd5c473e58852abf7ac4e46a7
Christian Antonsson <antonsson.christian@live.se>
2023-04-20 11:16:07 +0200

    Skapat en variabel för knappen i activity 1 och för textfältet som ska skicka vidare data till activity 2. Gjort en onClick funktion för knappen som startar activity 2.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+Button submitButton;
+EditText textField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton = findViewById(R.id.buttonSubmit);
+        textField = findViewById(R.id.editTextTextPersonName);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("name", textField.getText().toString()); // Optional
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -3,12 +3,16 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity2 extends AppCompatActivity {
+    TextView textOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        textOutput = findViewById(R.id.textView);
     }
 }
